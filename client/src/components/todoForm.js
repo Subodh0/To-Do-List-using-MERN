b178082899e86aaaffdfd4e330171180b288e41b
Subodh0 <70047623+Subodh0@users.noreply.github.com>
2023-09-16 14:32:49 +0530

    added server folder and updated client folder

--- client/src/components/todoForm.js
+++ client/src/components/todoForm.js
@@ -1,9 +1,23 @@
+import { useState } from "react"
+
 const ToDoForm =()=>{
+    const [text, settext] = useState("")
+
+    const onFormSubmit = () =>{
+
+    }
+
+    const onInputChange = (e) =>{
+        settext(e.target.value)
+    }
+
+
     return(
-        <form className="form">
+        <form className="form" onSubmit={onFormSubmit}>
             <input 
                 className="input"
                 placeholder="Enter a todo task..."
+                onChange={onInputChange}
             />
         </form>
     )
