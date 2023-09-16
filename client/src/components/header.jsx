4a958814cc8a2f6bbca4d491dc75462a3da089e7
Subodh0 <70047623+Subodh0@users.noreply.github.com>
2023-09-16 13:54:39 +0530

    header and form components added

---
+++ client/src/components/header.jsx
@@ -0,0 +1,9 @@
+const Hearder = ()=>{
+    return (
+        <header>
+            <h1>To-Do-List Using MERN</h1>
+        </header>
+    );
+}
+
+export default Hearder;
---
+++ client/src/components/todoForm.js
@@ -0,0 +1,11 @@
+const ToDoForm =()=>{
+    return(
+        <form className="form">
+            <input 
+                className="input"
+                placeholder="Enter a todo task..."
+            />
+        </form>
+    )
+}
+export default ToDoForm;
